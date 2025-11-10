2743a7a0b2aaf59a7c20d0cb4a53dba038a0484b
Basit46 <hassanbasitope@gmail.com>
2025-11-10 21:13:49 +0100

    feat: Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import RootLayoutContent from "./components/RootLayoutContent";
+import Head from "next/head";
 
 const inter = Inter({
   variable: "--inter",
@@ -12,28 +13,6 @@ export const metadata: Metadata = {
   title: "AlignHR",
   description:
     "Built for growing teams, it brings together HR operations, performance insights, and employee management.",
-  openGraph: {
-    url: "https://align-hr.vercel.app/",
-    type: "website",
-    title: "AlignHR",
-    description:
-      "Built for growing teams, it brings together HR operations, performance insights, and employee management.",
-    images: [
-      {
-        url: "https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png?token=wA1CMwma51D3ArYbULFo-LB3OgsvKBsu-MZGH4PH51I&height=584&width=1200&expires=33298805119",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "AlignHR",
-    description:
-      "Built for growing teams, it brings together HR operations, performance insights, and employee management.",
-    images: [
-      "https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png?token=wA1CMwma51D3ArYbULFo-LB3OgsvKBsu-MZGH4PH51I&height=584&width=1200&expires=33298805119",
-    ],
-    site: "https://align-hr.vercel.app",
-  },
 };
 
 export default function RootLayout({
@@ -43,6 +22,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>AlignHR</title>
+        <meta
+          name="description"
+          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
+        />
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://align-hr.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="AlignHR" />
+        <meta
+          property="og:description"
+          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png"
+        />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="align-hr.vercel.app" />
+        <meta property="twitter:url" content="https://align-hr.vercel.app/" />
+        <meta name="twitter:title" content="AlignHR" />
+        <meta
+          name="twitter:description"
+          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png"
+        />
+      </Head>
       <body className={`${inter.variable} min-h-screen bg-pry/10`}>
         <RootLayoutContent>{children}</RootLayoutContent>
       </body>
