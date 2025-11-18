f04ca9fa985dce80ee35224ac799487f6eafc673
Basit46 <hassanbasitope@gmail.com>
2025-11-18 11:00:13 +0100

    update: Metadata Img

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "AlignHR",
-  description:
-    "Built for growing teams, it brings together HR operations, performance insights, and employee management.",
+  description: "Built for growing teams.",
 };
 
 export default function RootLayout({
@@ -25,19 +24,13 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <title>AlignHR</title>
-        <meta
-          name="description"
-          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
-        />
+        <meta name="description" content="Built for growing teams." />
 
         {/* Open Graph */}
         <meta property="og:url" content="https://align-hr.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="AlignHR" />
-        <meta
-          property="og:description"
-          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
-        />
+        <meta property="og:description" content="Built for growing teams." />
         <meta
           property="og:image"
           content="https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png?token=moEykyaLjx6dttL2dHAf7U4qLEuNul6OH5aVajVDse8&height=584&width=1200&expires=33299459070"
@@ -48,10 +41,7 @@ export default function RootLayout({
         <meta property="twitter:domain" content="align-hr.vercel.app" />
         <meta property="twitter:url" content="https://align-hr.vercel.app/" />
         <meta name="twitter:title" content="AlignHR" />
-        <meta
-          name="twitter:description"
-          content="Built for growing teams, it brings together HR operations, performance insights, and employee management."
-        />
+        <meta name="twitter:description" content="Built for growing teams." />
         <meta
           name="twitter:image"
           content="https://opengraph.b-cdn.net/production/images/25a1c410-957f-4d4f-8ad5-b638f2c9a12d.png?token=moEykyaLjx6dttL2dHAf7U4qLEuNul6OH5aVajVDse8&height=584&width=1200&expires=33299459070"
